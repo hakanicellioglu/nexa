e3cffd8f4dc6358a95d521a813fe12249e69b168
Hakan Berke İÇELLİOĞLU <95232280+hakanicellioglu@users.noreply.github.com>
2025-10-07 10:49:54 +0300

    Wrap layout sections in container

--- dashboard.php
+++ dashboard.php
@@ -13,7 +13,7 @@ $companyCount = (int) $companyCountStmt->fetchColumn();
 $pageTitle = 'Panel - Nexa';
 include __DIR__ . '/header.php';
 ?>
-<div class="container-fluid">
+<div class="container">
     <div class="row">
         <?php include __DIR__ . '/sidebar.php'; ?>
         <main class="col-lg-10 ms-auto px-4 py-4">
